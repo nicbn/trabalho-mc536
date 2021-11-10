0c9b7c3a915cdb7c2d538218ca1c39bc8679c1d7
nicbn <nicolas.nattis@gmail.com>
2021-11-10 18:25:42 -0300

    Criação dos DataFrames

--- previa/src/build.py
+++ previa/src/build.py
@@ -1,5 +1,25 @@
 import pandas as pd
 
+drug_alias = pd.DataFrame(columns = ['DrugAlias', 'DrugId'])
+drug = pd.DataFrame(columns = ['DrugId', 'Name', 'Class'])
+source = pd.DataFrame(columns = [
+    'InteractionId',
+    'TrustLevel',
+    'Gene',
+    'GeneDrugInteraction',
+    'GeneDrugInteractionMechanism',
+    'GeneDiseaseInteraction',
+    'Source'
+])
+disease_alias = pd.DataFrame(columns = ['DiseaseAlias', 'DiseaseId'])
+disease = pd.DataFrame(columns = ['DiseaseId', 'Name', 'Class'])
+interaction = pd.DataFrame(columns = [
+    'InteractionId',
+    'DrugId',
+    'DiseaseId',
+    'InteractionType'
+])
+
 dgidb_interactions = pd.read_csv(
     '../data/external/dgidb/interactions.tsv.gz',
     sep = '\t',
@@ -13,4 +33,4 @@ disgenet_interactions = pd.read_csv(
     encoding = 'utf-8'
 )
 
-print(disgenet_interactions)
+print(drug_alias)
