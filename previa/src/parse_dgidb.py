75cf0a620a2ed03f273ca942a04b4f41704c0aa9
nicbn <nicolas.nattis@gmail.com>
2021-11-11 20:04:12 -0300

    format

--- previa/src/parse_dgidb.py
+++ previa/src/parse_dgidb.py
@@ -2,61 +2,59 @@ from typing import Dict, List
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 
-class DrugGenePair:
-    def __init__(self, drug: str, gene: str, drug_name: str):
-        self.gene = gene
-        self.drug = drug
-        self.drug_name = drug_name
-    def __eq__(self, x):
-        return self.gene == x.gene and self.drug == x.drug
-    def __hash__(self):
-        return hash(self.gene) ^ hash(self.drug)
-    def __repr__(self):
-        return str([self.gene, self.drug_name])
 
 class DrugGeneInteraction:
-    def __init__(self, ty: int, score: float, pmids: List[str]):
+    def __init__(self, drug: str, drug_name: str, ty: int, score: float, pmids: List[str]):
+        self.drug = drug
+        self.drug_name = drug_name
         self.ty = ty
         self.pmids = pmids
         self.score = score
+
     def __repr__(self):
         return str({
+            'drug': self.drug_name,
             'ty': self.ty,
             'pmids': self.pmids,
             'score': self.score
         })
 
-dgidb_interaction_types = {
-    "activator": 1,
-    "agonist": 1,
-    "binder": 1,
-    "chaperone": 1,
-    "inducer": 1,
-    "partial agonist": 1,
-    "potentiator": 1,
-    "stimulator": 1,
-    "substrate": 1,
 
-    "antagonist": 0,
-    "antibody": 0,
-    "antisense oligonucleotide": 0,
-    "blocker": 0,
-    "inhibitor": 0,
-    "inhibitory allosteric modulator": 0,
-    "inverse agonist": 0,
-    "partial antagonist": 0,
-    "suppressor": 0
+dgidb_interaction_types = {
+    'activator': 1,
+    'agonist': 1,
+    'binder': 1,
+    'chaperone': 1,
+    'inducer': 1,
+    'partial agonist': 1,
+    'potentiator': 1,
+    'stimulator': 1,
+    'substrate': 1,
+
+    'antagonist': 0,
+    'antibody': 0,
+    'antisense oligonucleotide': 0,
+    'blocker': 0,
+    'inhibitor': 0,
+    'inhibitory allosteric modulator': 0,
+    'inverse agonist': 0,
+    'partial antagonist': 0,
+    'suppressor': 0
 }
 
-def parse() -> Dict[DrugGenePair, DrugGeneInteraction]:
+
+def parse() -> Dict[str, List[DrugGeneInteraction]]:
+    # Indexado por gene
+
     dgidb = pd.read_csv(
         '../data/external/dgidb/interactions.tsv.gz',
-        sep = '\t',
-        compression = 'gzip',
-        encoding = 'utf-8')
+        sep='\t',
+        compression='gzip',
+        encoding='utf-8')
     dgidb = dgidb.dropna()
     scaler = MinMaxScaler()
-    dgidb[['interaction_group_score']] = scaler.fit_transform(dgidb[['interaction_group_score']])
+    dgidb[['interaction_group_score']] = scaler.fit_transform(
+        dgidb[['interaction_group_score']])
 
     r = {}
 
@@ -82,6 +80,11 @@ def parse() -> Dict[DrugGenePair, DrugGeneInteraction]:
         if ty == None:
             continue
 
-        pair = DrugGenePair(drug, gene, drug_name)
-        r[pair] = DrugGeneInteraction(ty, score, pmids)
+        arr = []
+        try:
+            arr = r[gene]
+        except KeyError:
+            r[gene] = arr
+
+        arr.append(DrugGeneInteraction(drug, drug_name, ty, score, pmids))
     return r
