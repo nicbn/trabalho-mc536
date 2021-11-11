97d63614d02fe7458495c82afa526eae66ae8964
nicbn <nicolas.nattis@gmail.com>
2021-11-10 21:07:17 -0300

    Código

--- previa/src/build.py
+++ previa/src/build.py
@@ -1,6 +1,51 @@
 import pandas as pd
 
+'''
 drug_alias = pd.DataFrame(columns = ['DrugAlias', 'DrugId'])
+disease_alias = pd.DataFrame(columns = ['DiseaseAlias', 'DiseaseId'])
+'''
+
+# gene_name
+# gene_claim_name
+# entrez_id
+# interaction_claim_source
+# interaction_types
+# drug_claim_name
+# drug_claim_primary_name
+# drug_name
+# drug_concept_id
+# interaction_group_score
+# PMIDs
+dgidb_interactions = pd.read_csv(
+    '../data/external/dgidb/interactions.tsv.gz',
+    sep = '\t',
+    compression = 'gzip',
+    encoding = 'utf-8')
+
+# geneId
+# geneSymbol
+# DSI
+# DPI
+# diseaseId
+# diseaseName
+# diseaseType
+# diseaseClass
+# diseaseSemanticType
+# score
+# EI
+# YearInitial
+# YearFinal
+# NofPmids
+# NofSnps
+# source
+disgenet_interactions = pd.read_csv(
+    '../data/external/disgenet/all_gene_disease_associations.tsv.gz',
+    sep = '\t',
+    compression = 'gzip',
+    encoding = 'utf-8'
+)
+
+disease = pd.DataFrame(columns = ['DiseaseId', 'Name', 'Class'])
 drug = pd.DataFrame(columns = ['DrugId', 'Name', 'Class'])
 source = pd.DataFrame(columns = [
     'InteractionId',
@@ -11,8 +56,6 @@ source = pd.DataFrame(columns = [
     'GeneDiseaseInteraction',
     'Source'
 ])
-disease_alias = pd.DataFrame(columns = ['DiseaseAlias', 'DiseaseId'])
-disease = pd.DataFrame(columns = ['DiseaseId', 'Name', 'Class'])
 interaction = pd.DataFrame(columns = [
     'InteractionId',
     'DrugId',
@@ -20,17 +63,7 @@ interaction = pd.DataFrame(columns = [
     'InteractionType'
 ])
 
-dgidb_interactions = pd.read_csv(
-    '../data/external/dgidb/interactions.tsv.gz',
-    sep = '\t',
-    compression = 'gzip',
-    encoding = 'utf-8')
-
-disgenet_interactions = pd.read_csv(
-    '../data/external/disgenet/all_gene_disease_associations.tsv.gz',
-    sep = '\t',
-    compression = 'gzip',
-    encoding = 'utf-8'
-)
+for row in dgidb_interactions:
+    pass
 
 print(drug_alias)
