9bfacf2fe9d849588fb6c6926b3c36b91e3656f4
nicbn <nicolas.nattis@gmail.com>
2021-11-11 17:32:06 -0300

    Atualização do código

--- previa/src/build.py
+++ previa/src/build.py
@@ -1,9 +1,5 @@
+from typing import *
 import pandas as pd
-
-'''
-drug_alias = pd.DataFrame(columns = ['DrugAlias', 'DrugId'])
-disease_alias = pd.DataFrame(columns = ['DiseaseAlias', 'DiseaseId'])
-'''
 
 # gene_name
 # gene_claim_name
@@ -42,28 +38,69 @@ disgenet_interactions = pd.read_csv(
     '../data/external/disgenet/all_gene_disease_associations.tsv.gz',
     sep = '\t',
     compression = 'gzip',
-    encoding = 'utf-8'
-)
+    encoding = 'utf-8')
 
-disease = pd.DataFrame(columns = ['DiseaseId', 'Name', 'Class'])
-drug = pd.DataFrame(columns = ['DrugId', 'Name', 'Class'])
-source = pd.DataFrame(columns = [
-    'InteractionId',
-    'TrustLevel',
-    'Gene',
-    'GeneDrugInteraction',
-    'GeneDrugInteractionMechanism',
-    'GeneDiseaseInteraction',
-    'Source'
-])
-interaction = pd.DataFrame(columns = [
+interactions = pd.DataFrame(columns = [
     'InteractionId',
     'DrugId',
     'DiseaseId',
-    'InteractionType'
-])
+    'Score',
+    'Gene',
+    'Type'])
+sources = pd.DataFrame(columns = [
+    'InteractionId',
+    'Kind',
+    'Score',
+    'Source'])
 
-for row in dgidb_interactions:
-    pass
+# Id => Name
+drugs = {}
+# Id => (Name, Class)
+diseases = {}
 
-print(drug_alias)
+all_interaction_types = ['inhibitor', 'activator', 'antagonist', 'inducer']
+
+for _, dgidb_row in dgidb_interactions.iterrows():
+    gene_name = dgidb_row['gene_name']
+    dgidb_interaction_types = list(filter(lambda x : x in all_interaction_types, str(dgidb_row['interaction_types']).split(',')))
+    if not dgidb_interaction_types:
+        continue
+
+    disease_interactions = disgenet_interactions.loc[disgenet_interactions['geneSymbol'] == gene_name]
+    if disease_interactions.empty:
+        continue
+
+    dgidb_score = dgidb_row['interaction_group_score']
+    drug_id = dgidb_row['drug_concept_id']
+    drug_name = dgidb_row['drug_name']
+
+    drugs[drug_id] = drug_name
+
+    for _, disgenet_row in disease_interactions.iterrows():
+        disease_id = disgenet_row['diseaseId']
+        disease_name = disgenet_row['diseaseName']
+        disease_class = disgenet_row['diseaseClass']
+        disgenet_score = disgenet_row['score']
+
+        diseases[disease_id] = (disease_name, disease_class)
+
+        ty = ''
+
+        id = interactions.size
+        interactions.append([
+            id,
+            drug_id,
+            disease_id,
+            dgidb_score * disgenet_score,
+            gene_name,
+            ty
+        ])
+
+        sources.append([
+            id,
+            'DGIDB',
+            dgidb_score,
+            f'https://dgidb.org/api/v2/interactions.json?genes={gene_name}&drugs={drug_id}'
+        ])
+
+    # drug_name = dgidb_row['drug_name']
