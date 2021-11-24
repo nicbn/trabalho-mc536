35bbe9dffa61870e7fac5653f96abd370d7c4160
nicbn <nicolas.nattis@gmail.com>
2021-11-24 02:19:58 -0300

    SQL -> notebooks

--- final/src/queries.py
+++
@@ -1,8 +0,0 @@
-## Pergunta 1
-query1 = "SELECT Dr.Name, Di.Name, I.Type, I.Score FROM Interaction as I, Drug as Dr, Disease  as Di WHERE Dr.Id = I.DrugId AND Di.Id = I.DiseaseId ORDER BY I.Score DESC LIMIT 10;"
-
-## Pergunta 2
-query2 = "SELECT D.Id, D.Name  FROM INTERACTION as I, DRUG as D, Disease  as Di  WHERE Di.id ='C0023449'  LIMIT 10;"
-
-## Pergunta 3
-query3 = ""
